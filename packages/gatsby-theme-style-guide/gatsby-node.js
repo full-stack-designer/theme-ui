9bb77a58b6907c58dbe601cb419a831f477797ee
Ivan Perez <ivan@Ivans-MacBook-Pro.local>
2020-02-12 20:34:26 +0100

    deleted original content

--- packages/gatsby-theme-style-guide/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createPages = ({ actions }, opts = {}) => {
-  const { basePath = '/style-guide' } = opts
-
-  actions.createPage({
-    path: basePath,
-    component: require.resolve('./src/template.js'),
-    context: {},
-  })
-}
